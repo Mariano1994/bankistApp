8d47884dfa93554cd89b389930450eeae2e596c4
Mariano <marianocapiliku@gmail.com>
2024-02-27 22:23:46 +0100

    Fixing the format of number

---
+++ challenges.js
@@ -0,0 +1,200 @@
+// Challenges
+
+const dogs = [
+  { weight: 22, curFood: 250, owner: ["Alice", "Bob"] },
+  { weight: 8, curFood: 200, owner: ["Matilda"] },
+  { weight: 13, curFood: 275, owner: ["Sarah", "John"] },
+  { weight: 32, curFood: 340, owner: ["Michael"] },
+];
+
+// 1
+dogs.forEach(
+  (dog) => (dog.recommendedFood = Math.trunc(dog.weight ** 0.75 * 28))
+);
+// console.log(dogs);
+
+/*
+Eating too much means the dog's current food portion is larger than the recommended portion, and eating to little is the
+opposit
+
+Eantin an okay amount means the dog's current food portion is within a range of 10% above and 10% below the recommended portion
+
+*/
+
+//2
+const sarahDog = dogs.find((dog) => dog.owner.includes("Sarah"));
+
+if (sarahDog.curFood > sarahDog.recommendedFood) {
+  console.log("Sarah's Dog is eating too much");
+} else {
+  console.log("Sarah's Dog is eating too little");
+}
+
+// 3
+const ownersEatTooMuch = dogs
+  .filter((dog) => dog.curFood > dog.recommendedFood)
+  .flatMap((owner) => owner.owner);
+console.log(ownersEatTooMuch);
+
+const ownersEatTooLitter = dogs
+  .filter((dog) => dog.curFood < dog.recommendedFood)
+  .flatMap((owner) => owner.owner);
+console.log(ownersEatTooLitter);
+
+//4
+
+//5
+const anyPerfectDog = dogs.some((dog) => dog.curFood === dog.recommendedFood);
+console.log(anyPerfectDog);
+
+// 6
+const anyOkDog = dogs.some(
+  (dog) =>
+    dog.curFood >
+    (dog.recommendedFood * 0.9 && dog.curFood < dog.recommendedFood * 1.1)
+);
+
+console.log(anyOkDog);
+
+// 7
+const okayDogs = dogs.filter(
+  (dog) =>
+    dog.curFood > dog.recommendedFood * 0.9 &&
+    dog.curFood < dog.recommendedFood * 1.1
+);
+console.log(okayDogs);
+
+// 8
+const dogsCopty = [...dogs];
+
+const sortedDogs = dogsCopty.sort(
+  (a, b) => a.recommendedFood - b.recommendedFood
+);
+
+console.log("======== NOT SORTED DOGS =================");
+console.log(dogs);
+console.log("========== SORTED DOGS");
+console.log(sortedDogs);
+
+// LECTURES
+
+console.log("CALL AND APPLY METHODS");
+
+const lufthansa = {
+  airline: "lufthansa",
+  aitaCode: "LH",
+  bookings: [],
+};
+
+const eurowings = {
+  airline: "Eurowings",
+  aitaCode: "EW",
+  bookings: [],
+};
+
+const swiss = {
+  airline: "Swiss Air Line",
+  aitaCode: "LX",
+  bookings: [],
+};
+// Function to book on one of the airlines services
+
+function book(flightNum, userName) {
+  console.log(
+    `${userName} booked a seat on ${this.airline} flight ${this.aitaCode}${flightNum}`
+  );
+
+  this.bookings.push({
+    flight: `${this.aitaCode}${flightNum}`,
+    userName,
+  });
+}
+
+// CALL METHOD
+
+book.call(eurowings, 2345, "Mariano Capiliku");
+book.call(eurowings, 2358, "Misael Lopes");
+book.call(lufthansa, 2334, "Mario Vicente");
+book.call(lufthansa, 2378, "Chelsea de Carvalho");
+book.call(swiss, 2790, "Scanner Afonso");
+
+// APPLY METHODS
+const flight = [2000, "Neri Lopes Capiliku"];
+book.apply(swiss, flight);
+
+// We can still using call with the spread operator
+book.call(eurowings, ...flight);
+
+// BIND METHOD
+const bookEW = book.bind(eurowings);
+const bookLU = book.bind(lufthansa);
+const bookSW = book.bind(swiss);
+
+bookEW(23, "Nataniela da Silva");
+bookLU(245, "Dawit Capiliku");
+bookSW(3545, "Marcos Aurelio");
+
+console.log(eurowings.bookings);
+console.log(lufthansa.bookings);
+console.log(swiss.bookings);
+
+console.log(eurowings);
+console.log(lufthansa);
+console.log(swiss);
+
+// FUNCTION RETURN A FUNCTION
+
+const addTaxRate = (rate) => {
+  return function (value) {
+    return value + value * rate;
+  };
+};
+
+const AddVat = addTaxRate(0.5);
+
+console.log(AddVat(100));
+console.log(AddVat(23));
+
+// CHALLENGE
+
+const answerBtn = document.getElementById("answer");
+
+const poll = {
+  question: "What is your favorite programing language?",
+  options: ["0: JavaScript", "1: Python", "2: Rust", "3: C++"],
+  ansewrs: new Array(4).fill(0),
+};
+
+function registeNewAnswer() {
+  const newAnswer = Number(
+    prompt(
+      `What is your favorite programing language?\n${this.options.join(
+        "\n"
+      )}\n(Write option number)`
+    )
+  );
+
+  if (newAnswer >= this.options.length) return;
+  this.ansewrs.map((item, index) => {
+    if (newAnswer === index) {
+      this.ansewrs[index] = item + 1;
+    }
+  });
+
+  return displayPollResults.call(this);
+}
+
+function displayPollResults(type = "array") {
+  if (type === "array") {
+    console.log(this.ansewrs);
+  }
+
+  if (type === "string") {
+    console.log(`poll results are : ${this.ansewrs.map((result) => result)}`);
+  }
+}
+
+const newAnserPoll = registeNewAnswer.bind(poll);
+answerBtn.addEventListener("click", newAnserPoll);
+
+// CLOSURES
--- script.js
+++ script.js
@@ -1,11 +1,22 @@
 "use strict";
-
 // Data
 const account1 = {
   owner: "Jonas Schmedtmann",
-  movements: [200, 450, -400, 3000, -650, -130, 70, 1300],
+  movements: [200, 455.23, -306.5, 25000, -642.21, -133.9, 79.97, 1300],
   interestRate: 1.2, // %
   pin: 1111,
+  movementsDates: [
+    "2019-11-18T21:31:17.178Z",
+    "2019-12-23T07:42:02.383Z",
+    "2020-01-28T09:15:04.904Z",
+    "2020-04-01T10:17:24.185Z",
+    "2020-05-08T14:11:59.604Z",
+    "2020-05-27T17:01:17.194Z",
+    "2020-07-11T23:36:17.929Z",
+    "2020-07-12T10:51:36.790Z",
+  ],
+  currency: "EUR",
+  locale: "pt-PT",
 };
 
 const account2 = {
@@ -13,23 +24,21 @@ const account2 = {
   movements: [5000, 3400, -150, -790, -3210, -1000, 8500, -30],
   interestRate: 1.5,
   pin: 2222,
+  movementsDates: [
+    "2019-11-01T13:15:33.035Z",
+    "2019-11-30T09:48:16.867Z",
+    "2019-12-25T06:04:23.907Z",
+    "2020-01-25T14:18:46.235Z",
+    "2020-02-05T16:33:06.386Z",
+    "2020-04-10T14:43:26.374Z",
+    "2020-06-25T18:49:59.371Z",
+    "2020-07-26T12:01:20.894Z",
+  ],
+  currency: "USD",
+  locale: "en-US",
 };
 
-const account3 = {
-  owner: "Steven Thomas Williams",
-  movements: [200, -200, 340, -300, -20, 50, 400, -460],
-  interestRate: 0.7,
-  pin: 3333,
-};
-
-const account4 = {
-  owner: "Sarah Smith",
-  movements: [430, 1000, 700, 50, 90],
-  interestRate: 1,
-  pin: 4444,
-};
-
-const accounts = [account1, account2, account3, account4];
+const accounts = [account1, account2];
 
 // Elements
 const labelWelcome = document.querySelector(".welcome");
@@ -70,7 +79,9 @@ const displayMoviments = (movements, sort = false) => {
       index + 1
     } ${type}</div>
                       <div class="movements__date">3 days ago</div>
-                      <div class="movements__value">${movement}€</div>
+                      <div class="movements__value">${movement.toFixed(
+                        2
+                      )}€</div>
                     </div>`;
 
     containerMovements.insertAdjacentHTML("afterbegin", html);
@@ -114,7 +125,7 @@ const calculateInterest = (acc) => {
 
 // Function to display any information into the DOM
 const display = (value, element) => {
-  element.textContent = `${value}€`;
+  element.textContent = `${value.toFixed(2)}€`;
 };
 
 // Calling the display function to display different values into the DOM
@@ -220,9 +231,10 @@ btnClose.addEventListener("click", (event) => {
   inputClosePin.value = "";
 });
 
+// FUNCTION TO GET LOAN
 btnLoan.addEventListener("click", (event) => {
   event.preventDefault();
-  const amount = Number(inputLoanAmount.value);
+  const amount = Math.floor(inputLoanAmount.value);
   if (
     amount > 0 &&
     currentAccount.movements.some((mov) => mov >= amount * 0.1)
