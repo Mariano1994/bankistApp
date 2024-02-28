7e7e7299beff32bf7e695c50fcace40333e71f90
Mariano <marianocapiliku@gmail.com>
2024-02-28 03:20:33 +0100

    Create current Date functinality And display movements date

--- script.js
+++ script.js
@@ -67,18 +67,29 @@ const inputCloseUsername = document.querySelector(".form__input--user");
 const inputClosePin = document.querySelector(".form__input--pin");
 
 // FUNCTION TO DISPLAY ALL THE MOVIMENTS
-const displayMoviments = (movements, sort = false) => {
+const displayMoviments = (account, sort = false) => {
   containerMovements.innerHTML = "";
 
-  const movs = sort ? movements.slice().sort((a, b) => a - b) : movements;
+  const movs = sort
+    ? account.movements.slice().sort((a, b) => a - b)
+    : account.movements;
   movs.forEach((movement, index) => {
     const type = movement > 0 ? "deposit" : "withdrawal";
+
+    const date = new Date(account.movementsDates[index]);
+    const now = new Date();
+    const day = `${date.getDate()}`.padStart(2, 0);
+    const month = `${date.getMonth() + 1}`.padStart(2, 0);
+    const year = date.getFullYear();
+
+    const displayDate = `${day}/${month}/${year}`;
     const html = ` 
     <div class="movements__row">
                       <div class="movements__type movements__type--${type}">${
       index + 1
     } ${type}</div>
-                      <div class="movements__date">3 days ago</div>
+                      <div class="movements__date">${displayDate}</div>
+                      
                       <div class="movements__value">${movement.toFixed(
                         2
                       )}€</div>
@@ -146,7 +157,7 @@ createUserName(accounts);
 
 // Fucntion to Update the UI
 function updateUI(acc) {
-  displayMoviments(acc.movements);
+  displayMoviments(acc);
   display(calculateBalance(acc), labelBalance);
   display(calculateIncomes(acc), labelSumIn);
   display(calculateOutcomes(acc), labelSumOut);
@@ -165,6 +176,16 @@ btnLogin.addEventListener("click", (event) => {
   if (currentAccount?.pin === Number(inputLoginPin.value)) {
     labelWelcome.textContent = `Welcome, ${currentAccount.owner.split(" ")[0]}`;
 
+    // Create current Date
+    const now = new Date();
+    const day = `${now.getDate()}`.padStart(2, 0);
+    const month = `${now.getMonth() + 1}`.padStart(2, 0);
+    const year = now.getFullYear();
+    const hours = `${now.getHours()}`.padStart(2, 0);
+    const minutes = `${now.getMinutes()}`.padStart(2, 0);
+    labelDate.textContent = `${day}/${month}/${year}, ${hours}:${minutes}`;
+
+    // SHOW THE UI
     containerApp.style.opacity = 100;
     //Update UI
     updateUI(currentAccount);
@@ -250,6 +271,6 @@ btnLoan.addEventListener("click", (event) => {
 let sorted = false;
 btnSort.addEventListener("click", (event) => {
   event.preventDefault();
-  displayMoviments(currentAccount.movements, !sorted);
+  displayMoviments(currentAccount, !sorted);
   sorted = !sorted;
 });
